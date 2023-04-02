53e745622597abc4871895b2d1899c632e40bae9
gaurav-vishwa <107577366+gaurav-vishwa@users.noreply.github.com>
2023-04-02 19:04:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Contact } from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
